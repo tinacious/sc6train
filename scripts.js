8ef5a9a961cd24a39d966d35d0ee892c246c08f8
Tinacious Design <tinacious.design@gmail.com>
2012-10-17 10:09:57 -0400

    refine captions

--- scripts.js
+++ scripts.js
@@ -48,9 +48,11 @@ $(document).ready(function(){
 
 	// Automagically generate image captions and titles using alt tags
 	$('#content img:not([class=new_win])').each(function(){
-		var imgCaption = $(this).attr('alt');
-		$(this).after("<span class='caption'>" + imgCaption + "</span>");
-		$(this).attr('title', imgCaption);
+		if($('#content img').attr('alt')) {
+			var imgCaption = $(this).attr('alt');
+			$(this).after("<span class='caption'>" + imgCaption + "</span>");
+			$(this).attr('title', imgCaption);	
+		}
 	});
 
 	// Go-to Table of Contents button control for desktop
